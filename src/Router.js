9a2b86bf8b4526bdf16a8cfaa9f4f774bb046a98
SeanKim05 <103613612+SeanKim05@users.noreply.github.com>
2022-09-20 13:25:04 +0900

    UI 작업완료

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Main from "./pages/Main/Main";
+import DropDown from "./components/Header/DropDown/DropDown";
 
 const { BrowserRouter, Routes, Route } = require("react-router-dom");
 const { Reset } = require("styled-reset");
@@ -12,6 +13,7 @@ function Router() {
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/dropdown" element={<DropDown />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import "./MyInfo/MyInfo";
+
 import styles from "./Header.module.scss";
+import MyInfo from "./MyInfo/MyInfo";
 
 function Header() {
-  return <div className={styles.container}></div>;
+  const [loggedIn, setLoggedIn] = useState(true);
+
+  return (
+    <div className={styles.container}>
+      <section className={styles.header_border}>
+        <div className={styles.header_wrapper}>
+          <nav className={styles.user_info_nav}>
+            {loggedIn ? <MyInfo /> : <span>회원가입</span>}
+          </nav>
+
+          <div className={styles.header_logo_wrapper}>
+            <img
+              className={styles.header_logo}
+              src="https://d6j35gv9ux3qi.cloudfront.net/image/new-header-logo.svg"
+              alt="header logo"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Header;
