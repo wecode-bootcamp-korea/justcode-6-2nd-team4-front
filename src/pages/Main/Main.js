1a10b1c7e2570424f1d2f80f66c23cacdf050e4c
Joey <seyoung1st@gmail.com>
2022-09-21 00:15:00 +0900

    Feature: 메인페이지 이미지슬라이더 구현
    swiper 라이브러리를 이용한 carousel 구현
    
    Issue: #7

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,10 +1,12 @@
 import styles from './Main.module.scss';
 import Navigation from '../../components/NavigationBar/NavigationBar';
+import SimpleSlider from '../../components/MainImageSlider/ImageSlider';
 
 function Main() {
   return (
     <div>
       <Navigation />
+      <SimpleSlider />
     </div>
   );
 }
