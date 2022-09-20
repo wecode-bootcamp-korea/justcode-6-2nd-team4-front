6e34eba65b37db568c8e071330d579e06799dd01
Joey <seyoung1st@gmail.com>
2022-09-20 12:49:41 +0900

    UI: DropDown Menu 레이아웃 추가
    디자인마켓, 맞춤제작소 카테고리 메뉴 레이아웃 추가 및 Drop down 애니메이션 구현
    
    Issue: #2

---
+++ src/components/Nav/DropDownMenu/WorkMenu.js
@@ -0,0 +1,40 @@
+import styles from "./DropDownMenu.module.scss";
+import carpentry from "../../../assets/images/carpentry.png";
+import leather from "../../../assets/images/leather.png";
+import jewelry from "../../../assets/images/jewelry.png";
+import pottery from "../../../assets/images/pottery.png";
+import tools from "../../../assets/images/tools.png";
+
+function WorkMenu({ workMenuOpen }) {
+  return (
+    <div
+      className={
+        workMenuOpen ? `${styles.activate}` : `${styles.beforeactivate}`
+      }
+    >
+      <div className={styles.container}>
+        <div className={styles.box}>
+          <img src={carpentry} alt="목공방 카테고리 이미지"></img>
+          <div>목공방</div>
+        </div>
+        <div className={styles.box}>
+          <img src={leather} alt="가죽공방 카테고리 이미지"></img>
+          <div>가죽공방</div>
+        </div>
+        <div className={styles.box}>
+          <img src={jewelry} alt="금속공방 카테고리 이미지"></img>
+          <div>금속공방</div>
+        </div>
+        <div className={styles.box}>
+          <img src={pottery} alt="도예공방 카테고리 이미지"></img>
+          <div>도예공방</div>
+        </div>
+        <div className={styles.box}>
+          <img src={tools} alt="기타공방 카테고리 이미지"></img>
+          <div>기타공방</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default WorkMenu;
--- src/components/Nav/NavigationBar.js
+++ src/components/Nav/NavigationBar.js
@@ -1,14 +1,55 @@
 import DropDownMenu from "./DropDownMenu/DropDownMenu";
+import WorkMenu from "./DropDownMenu/WorkMenu";
 import styles from "./NavigationBar.module.scss";
+import React, { useState } from "react";
 
 function Navigation() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [workMenuOpen, setWorkMenuOpen] = useState(false);
+  const [fontColor, setFontColor] = useState(true);
+  const handleMouseEnter = () => {
+    setMenuOpen(true);
+  };
+  const handleMouseLeave = () => {
+    setMenuOpen(false);
+  };
+  const handleMouseEnterWorkMenu = () => {
+    setWorkMenuOpen(true);
+  };
+  const handleMouseLeaveWorkMenu = () => {
+    setWorkMenuOpen(false);
+  };
+
+  let fontStyle = {
+    color: "rgb(3, 179, 238)",
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <div className={styles.box}>디자인 마켓</div>
-        <div className={styles.box}>맞춤 제작소</div>
+        <div
+          className={styles.box}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onClick={() => {
+            setFontColor(true);
+          }}
+          style={fontColor ? fontStyle : null}
+        >
+          디자인 마켓
+        </div>
+        <div
+          className={styles.box}
+          onMouseEnter={handleMouseEnterWorkMenu}
+          onMouseLeave={handleMouseLeaveWorkMenu}
+          onClick={() => setFontColor(false)}
+          style={fontColor ? null : fontStyle}
+        >
+          맞춤 제작소
+        </div>
       </div>
-      <DropDownMenu />
+      <DropDownMenu menuOpen={menuOpen} />
+      <WorkMenu workMenuOpen={workMenuOpen} />
     </div>
   );
 }
