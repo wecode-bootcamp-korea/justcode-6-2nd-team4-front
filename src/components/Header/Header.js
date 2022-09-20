39abbd175a1e2c89942c19c5419ae40d803cbcac
KimYuHyun <103613612+SeanKim05@users.noreply.github.com>
2022-09-20 16:28:29 +0900

    Feature: Header, Footer UI (#8)
    * UI 작업완료
    
    * Modify: header 코드
    
    * Fix: Nav바 간섭 수정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,48 @@
-import styles from "./Header.module.scss";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import styles from './Header.module.scss';
+import MyInfo from './MyInfo/MyInfo';
+import Navigation from '../../pages/Main/Nav/NavigationBar';
 
 function Header() {
-  return <div className={styles.container}></div>;
+  const [loggedIn, setLoggedIn] = useState(true);
+
+  const navigate = useNavigate;
+  const goMain = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className={styles.container}>
+      <section className={styles.header_border}>
+        <div className={styles.header_wrapper}>
+          <nav className={styles.user_info_nav}>
+            {loggedIn ? (
+              <MyInfo />
+            ) : (
+              <span>
+                <span>로그인</span>
+                <span>회원가입</span>
+              </span>
+            )}
+          </nav>
+
+          <div className={styles.header_logo_wrapper}>
+            <img
+              className={styles.header_logo}
+              src="https://d6j35gv9ux3qi.cloudfront.net/image/new-header-logo.svg"
+              alt="header logo"
+              onClick={goMain}
+            />
+          </div>
+          {/* <div>
+            <Navigation />
+          </div> */}
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Header;
