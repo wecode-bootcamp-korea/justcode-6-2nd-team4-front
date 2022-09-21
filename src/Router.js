e0a31b771a7153eed633aaddb05b83da2f17fc88
seyoung97 <108709932+seyoung97@users.noreply.github.com>
2022-09-21 15:55:24 +0900

    Feature: 메인페이지 이미지슬라이더 구현 (#13)
    - Structure: NavigationBar 파일 이동
    - Feature: 메인페이지 이미지슬라이더 구현
    - Modify: 네브바 및 이미지슬라이더 코드 수정
    
    Issue:#7

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import HelmetProvider from './components/HelmetProvider/HelmetProvider';
+import Navigation from './components/NavigationBar/NavigationBar';
 import Main from './pages/Main/Main';
 import Signup from './pages/Signup/Signup';
 import GlobalStyles from './styles/GlobalStyles';
@@ -13,6 +14,7 @@ function Router() {
       <HelmetProvider />
       <GlobalStyles />
       <Header />
+      <Navigation />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/signup" element={<Signup />} />
