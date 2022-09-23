f93b8c8cb79169fb46e6d249d82439d274e2f051
KimYuHyun <103613612+SeanKim05@users.noreply.github.com>
2022-09-23 16:28:35 +0900

    Feature: 로그인 회원가입 통신 추가 (#21)
    * Feature: 로그인& 회원가입 UI (#15)
        - UI: 로그인 회원가입 완료
        - 로그인 창 모달 기능 추가
        - 유저이름 표시 기능 추가
        - 서버 통신기능 추가
        Issue:#15

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import styles from './Header.module.scss';
@@ -6,24 +7,38 @@ import MyInfo from './MyInfo/MyInfo';
 import logo from '../../../src/assets/images/logo.png';
 
 function Header() {
-  const [loggedIn, setLoggedIn] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const tokenStatus = localStorage.getItem('token');
+
+    if (tokenStatus) {
+      tokenStatus === null ? setLoggedIn(false) : setLoggedIn(true);
+    }
+  });
 
   const navigate = useNavigate();
+  const goLogin = () => {
+    navigate('/login');
+  };
   const goMain = () => {
     navigate('/');
+  };
+  const goSignup = () => {
+    navigate('/signup');
   };
 
   return (
     <div className={styles.container}>
       <section className={styles.header_border}>
-        <nav className={styles.user_info_nav}>
+        <nav className={styles.user_info}>
           {loggedIn ? (
             <MyInfo />
           ) : (
-            <span>
-              <span>로그인</span>
-              <span>회원가입</span>
-            </span>
+            <div className={styles.user_info_nav}>
+              <div onClick={goLogin}>로그인</div>
+              <div onClick={goSignup}>회원가입</div>
+            </div>
           )}
         </nav>
         <div className={styles.header_wrapper}>
--- src/components/Header/MyInfo/MyInfo.js
+++ src/components/Header/MyInfo/MyInfo.js
@@ -1,8 +1,18 @@
-import React from "react";
-import styles from "./MyInfo.module.scss";
-import DropDown from "../DropDown/DropDown";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import styles from './MyInfo.module.scss';
+import DropDown from '../DropDown/DropDown';
 
 function MyInfo() {
+  const [userName, setUserName] = useState('사용자님');
+
+  useEffect(() => {
+    const nameStatus = localStorage.getItem('userName');
+    if (nameStatus) {
+      nameStatus === null ? setUserName('사용자님') : setUserName(nameStatus);
+    }
+  }, []);
+
   return (
     <div className={styles.container}>
       <nav className={styles.user_info_nav}>
@@ -10,7 +20,7 @@ function MyInfo() {
           alt="프로필"
           src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png"
         />
-        <DropDown title="사용자님" />
+        <DropDown title={userName} />
         <div>고객센터</div>
       </nav>
     </div>
