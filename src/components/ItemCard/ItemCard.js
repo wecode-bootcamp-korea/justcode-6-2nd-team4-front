10686d8b0884b5b3790fbc518333cd764f755d63
seyoung97 <108709932+seyoung97@users.noreply.github.com>
2022-09-23 14:22:16 +0900

    Feature: 메인화면 통신 (#18)
    - Feature: 메인화면 통신
    - UI: 메인화면 커서 수정
    
    Issue:#17

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -7,7 +7,7 @@ function ItemCard({ data }) {
   return (
     <div className={styles.card_wrapper}>
       <div className={styles.card_img_box}>
-        <img src={furniture}></img>
+        <img src={data.image}></img>
       </div>
       <div className={styles.card_store_type}>
         <div className={styles.card_store_name}>{data.shop}</div>
@@ -15,12 +15,15 @@ function ItemCard({ data }) {
       </div>
       <div className={styles.card_product_name}>{data.product_name}</div>
       <div className={styles.card_product_price}>{data.price}</div>
-      <div className={styles.card_review}>
-        <img src={star}></img>
-        <span>
-          {data.average}
-          {data.review_count}
-        </span>
+      <div className={styles.review_delivery_container}>
+        <div className={styles.card_review}>
+          <img src={star}></img>
+          <span>{data.average !== null ? data.average : '0.0'}</span>
+          <span>({data.review_count !== null ? data.review_count : '0'})</span>
+        </div>
+        {data.delivery_type == null ? (
+          <div className={styles.delivery_type}>무료배송</div>
+        ) : null}
       </div>
     </div>
   );
