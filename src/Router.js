4faae739996d48d0581dd2467d31f1992bbaf1ab
seyoung97 <108709932+seyoung97@users.noreply.github.com>
2022-09-26 15:38:55 +0900

    Feature/category (#22)
    - UI: 카테고리 화면 레이아웃 완성
    - Feature: 더보기 기능, 추천순 필터링 완성
    - Feature: 카테고리 필터링
    - Feature:무한스크롤(미완성)
    Issue: #20

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import HelmetProvider from './components/HelmetProvider/HelmetProvider';
 import Navigation from './components/NavigationBar/NavigationBar';
+import Category from './pages/Category/Category';
 import Main from './pages/Main/Main';
 import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
@@ -19,6 +20,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/themeCategory/:id" element={<Category />} />
         <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
