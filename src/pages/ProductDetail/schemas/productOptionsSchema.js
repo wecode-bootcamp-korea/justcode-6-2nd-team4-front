3ab7e8316dd42a18cf9d8257d23ba40e597357e5
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-09-28 21:52:30 +0900

    Feature: 제품 상세 화면 작업 (#19)
    제품 상세 화면 작업
      - mock, assets 데이터 추가
      - router 경로 추가
      - json-data-mapping 라이브러리 및 스키마 추가
      - redux 적용 (제품, 옵션, 선택한 옵션)
      - 컴포넌트 분리
      - 서버 포트 변경
      - API 연동
    
    Issue: #1

---
+++ src/pages/ProductDetail/schemas/productOptionsSchema.js
@@ -0,0 +1,15 @@
+export const productOptionsSchema = {
+  isList: true,
+  data: '',
+  format: {
+    id: {
+      sourceField: 'id',
+    },
+    title: {
+      sourceField: 'title',
+    },
+    detail: {
+      sourceField: 'detail',
+    },
+  },
+};
